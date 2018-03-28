c9dfa07d857d3c1ac67255fc3d72f6fb088a9bfa
Richard Petrie <deedasmi@gmail.com>
2018-03-28 18:13:38 -0500

    Catch configuration issues for plugins
    Need to log plugin panics once logger is implemented

--- agent/plugins/src/plugin_initialization.rs
+++ agent/plugins/src/plugin_initialization.rs
@@ -3,8 +3,19 @@ macro_rules! plugins {
         $(extern crate $x;)*
 
         pub fn init() -> Vec<Box<AgentPlugin>> {
+            use std::panic;
             let mut v: Vec<Box<AgentPlugin>> = vec!();
-            $(v.push(Box::new($x::new()));)*
+            let p = panic::take_hook();
+            panic::set_hook(Box::new(|_info| {
+                // do nothing
+            }));
+            $(
+                match std::panic::catch_unwind(|| $x::new()) {
+                    Ok(x) => {v.push(Box::new(x))},
+                    Err(_) => { }
+                };
+            )*
+            panic::set_hook(p);
             v
         }
     }
